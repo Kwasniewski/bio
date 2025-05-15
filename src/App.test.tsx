d2165ff836700ea6ff4eb0759b72c01a2a69e2c8
Joey Kwasniewski <kwasniewski@gmail.com>
2025-05-15 15:54:12 -0700

    fix: fix tests and coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('name displays', () => {
   render(<App />);
   const linkElement = screen.getByText(/Joseph Kwasniewski/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('background color changes on mode switch', () => {
+  render(<App />);
+  const linkElement = screen.getByLabelText(/Toggle dark mode/i);
+  fireEvent.click(linkElement);
+  const theme = document.documentElement.getAttribute('data-theme');
+  fireEvent.click(linkElement);
+  const newTheme = document.documentElement.getAttribute('data-theme');
+  expect(theme).not.toEqual(newTheme);
 });
--- src/components/Skills.test.tsx
+++ src/components/Skills.test.tsx
@@ -3,6 +3,6 @@ import Skills from './Skills';
 
 test('skills displays', () => {
   render(<Skills />);
-  const linkElement = screen.getByText(/JavaScript/i);
+  const linkElement = screen.getByText(/TypeScript/i);
   expect(linkElement).toBeInTheDocument();
 });
